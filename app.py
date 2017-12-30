017ac48d78ff9404076cc0e9752a9d3c6c49ba95
Faldi <13516098@std.stei.itb.ac.id>
2017-12-30 17:29:57 +0700

    multicast in form tandu

--- app.py
+++ app.py
@@ -182,13 +182,6 @@ def handle_text_message(event):
                 TextSendMessage(text='Pesan telah terkirim')
             )
         elif text.startswith('balas-' + nama_tandu_1):
-            line_bot_api.push_message(user_id_tandu_1, [ImageSendMessage(
-                original_content_url=profile.picture_url, preview_image_url=profile.picture_url
-                ),
-                TextSendMessage(text='Menerima balasan dari ' + profil.display_name + ':\n' + text[(6+len(nama_tandu_1)) : ]),
-                TextSendMessage(text='Untuk membalas pesan di atas, silahkan gunakan format di bawah:'),
-                TextSendMessage(text='balas_admin-' + profile.user_id + ':\n<pesan Anda>')
-            ])
             line_bot_api.push_message(user_id_admin, [ImageSendMessage(
                 original_content_url=profile.picture_url, preview_image_url=profile.picture_url
                 ),
@@ -472,27 +465,7 @@ def handle_text_message(event):
 
     elif text.startswith('[Form Peminjaman Tandu]'):
         profile = line_bot_api.get_profile(event.source.user_id)
-        line_bot_api.push_message(user_id_tandu_1, [
-            ImageSendMessage(
-                original_content_url=profile.picture_url, preview_image_url=profile.picture_url
-            ),
-            TextSendMessage(
-                text=text + '\nDisplay Name: ' + profile.display_name
-            ),
-            TextSendMessage(text='Untuk membalas pesan di atas, silahkan gunakan format di bawah:'),
-            TextSendMessage(text='balas_admin-' + profile.user_id + ':\n<pesan Anda>')
-        ])
-        line_bot_api.push_message(user_id_tandu_2, [
-            ImageSendMessage(
-                original_content_url=profile.picture_url, preview_image_url=profile.picture_url
-            ),
-            TextSendMessage(
-                text=text + '\nDisplay Name: ' + profile.display_name
-            ),
-            TextSendMessage(text='Untuk membalas pesan di atas, silahkan gunakan format di bawah:'),
-            TextSendMessage(text='balas_admin-' + profile.user_id + ':\n<pesan Anda>')
-        ])
-        line_bot_api.push_message(user_id_admin, [
+        line_bot_api.multicast([user_id_tandu_1, user_id_tandu_2, user_id_admin], [
             ImageSendMessage(
                 original_content_url=profile.picture_url, preview_image_url=profile.picture_url
             ),
